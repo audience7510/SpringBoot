6d30bb61f774dd71bc0ab1f3d18e31ae836636ca
shiyongming <shiyongming@opssino.com>
2018-09-27 18:10:38 +0800

    整合redis,并使用Jackson序列化

--- admin/src/main/java/com/audience/admin/admin/Controller/UserController.java
+++ admin/src/main/java/com/audience/admin/admin/Controller/UserController.java
@@ -1,28 +1,32 @@
 package com.audience.admin.admin.Controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
-import com.audience.api.api.model.User;
 import com.audience.api.api.service.UserService;
-import org.springframework.stereotype.Component;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Component
+import javax.annotation.Resource;
+
+@RestController
 @RequestMapping("/audience")
 public class UserController {
 
     @Reference
     UserService userService;
 
-
+    @Resource
+    RedisTemplate redisTemplate;
 
     @RequestMapping("/getUser")
     @ResponseBody
-    public User getUser(String userName){
-        User user = userService.getUser(userName);
+    public String getUser(String userName){
+//        User user = userService.getUser(userName);
 //        User user = new User();
 //        user.setName("heihei");
 //        user.setAge(18);
-        return user;
+        redisTemplate.opsForValue().set(userName,1111323232);
+        return "ok";
     }
 }
