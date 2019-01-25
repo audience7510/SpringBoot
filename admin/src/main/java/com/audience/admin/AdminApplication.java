74c5e736c465f25d34fd866f55defbd0a99c3fd1
Administrator <audience7510@163.com>
2019-01-25 15:54:44 +0800

    引入mq依赖及properties配置

--- admin/src/main/java/com/audience/admin/AdminApplication.java
+++ admin/src/main/java/com/audience/admin/AdminApplication.java
@@ -1,12 +1,16 @@
 package com.audience.admin;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AdminApplication {
+	private static final Logger logger = LoggerFactory.getLogger(AdminApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(AdminApplication.class, args);
+		logger.info("admin服务启动完成");
 	}
 }
